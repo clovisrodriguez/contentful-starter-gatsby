2d8b62701a7536b92ba24d33c546e108dbb2ceff
clovis <clovis.rodriguez@globant.com>
2020-09-15 19:50:03 -0500

    update title and favicon

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,9 @@ import Logo from '../images/white_logo.png'
 export default function Home() {
   return (
     <div className="root">
+      <head>
+        <title>Enciso Montero</title>
+      </head>
       <div>
         <img src={Logo} />
         <h3>Muy pronto, Nuestro negocio es crecer tu negocio</h3>
